1e3524978b3229619085f51b0363c8857cf6a275
Abraham John Paul <64521674+abraham-john-paul@users.noreply.github.com>
2020-12-30 22:57:40 -0800

    Update automation_constants.py

--- automation_constants.py
+++ automation_constants.py
@@ -1,15 +1,8 @@
 import enum
-
-class WorkingStatus(enum.Enum): 
-    OnLeave = 1
-    WorkFromHome = 2
-    WorkFromOffice = 3
-    BusinessTrip = 4
-    WeekendsOrHolidays = 5
     
 class AarogyaSetuStatus(enum.Enum): 
     Safe = 2
-    Unsafe = 3
+    UnSafe = 3
 
 class NetworkConnectionType(enum.Enum): 
     Broadband = 2
